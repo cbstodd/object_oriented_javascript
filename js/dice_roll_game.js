9cc3d3e07165bb9e11bf3bf350faee4d3387ecec
Colin Stodd <cbstodd@gmail.com>
2015-10-14 21:33:23 -0400

    Improved dice roll game. Now two dice and a clear button

--- js/dice_roll_game.js
+++ js/dice_roll_game.js
@@ -1,11 +1,29 @@
-//CREATES THE RANDOM NUMBER/DICE GAME.
-// var dice = {
-//   sides: 6,
-//   roll: function() {
-//     var randomNumber = Math.floor(Math.random() * this.sides + 1);
-//     return (randomNumber);
-//   }
-// };
+//PRINT TO PAGE FUNCTION.
+function printNumberOne(number) {
+  var dicePlaceHolderOne = document.getElementById("dicePlaceHolderOne");
+  dicePlaceHolderOne.innerHTML = number;
+}
+
+function printNumberTwo(number) {
+  var dicePlaceHolderTwo = document.getElementById("dicePlaceHolderTwo");
+  dicePlaceHolderTwo.innerHTML = number;
+}
+
+var diceBtnOne = document.getElementById("diceBtnOne");
+var diceBtnTwo = document.getElementById("diceBtnTwo");
+var clearDice = document.getElementById("clearDice");
+
+diceBtnOne.onclick = function() {
+  var rollOne = diceOne.roll();
+  printNumberOne("<h1>" + rollOne + "</h1>");
+
+};
+diceBtnTwo.onclick = function() {
+  var rollTwo = diceTwo.roll();
+  printNumberTwo("<h1>" + rollTwo + "</h1>");
+
+};
+
 
 //CREATES THE RANDOM NUMBER/DICE VIA OBJECT CONSTRUCTOR.
 function Dice(sides) {
@@ -14,36 +32,16 @@ function Dice(sides) {
     var randomNumber = Math.floor(Math.random() * this.sides + 1);
     return randomNumber;
   };
-
 }
+var diceOne = new Dice(6);
+var diceTwo = new Dice(6);
 
-  var diceOne = new Dice(6);
-  var diceTwo = new Dice(6);
+//JQUERY CLEARS DICE NUMBERS.
+$(document).ready(function() {
+  $('#clearDice').click(function() {
+    $("#dicePlaceHolderOne, #dicePlaceHolderTwo").each(function(index, element) {
+      element.innerHTML = '';
+    });
+  });
 
-
-//PRINT TO PAGE FUNCTION.
-function printNumber1(number) {
-  var dicePlaceHolderOne = document.getElementById("dicePlaceHolderOne");
-  dicePlaceHolderOne.innerHTML = number;
-
-}
-
-function printNumber2(number) {
-  var dicePlaceholderTwo = document.getElementById("dicePlaceholderTwo");
-  dicePlaceholderTwo.innerHTML = number;
-
-}
-
-var diceBtnOne = document.getElementById("diceBtnOne");
-var diceBtnTwo = document.getElementById("diceBtnTwo");
-
-diceBtnOne.onclick = function() {
-  var rollOne = diceOne.roll();
-  printNumber1("<h1>" + rollOne + "</h1>");
-
-};
-
-diceBtnTwo.onclick = function() {
-  var rollTwo = diceTwo.roll();
-  printNumber2("<h1>" + rollTwo + "</h1>");
-};
+});//END jQuery
