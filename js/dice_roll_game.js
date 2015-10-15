24dad6a53364f154dc1eea8d3bebd94017bb986a
Colin Stodd <cbstodd@gmail.com>
2015-10-15 17:24:22 -0400

    Finished Dice Roll Game

--- js/dice_roll_game.js
+++ js/dice_roll_game.js
@@ -15,17 +15,16 @@ function printNumberTwo(number) {
 }
 
 
-
-//CREATES A RANDOM NUMBER.
-function diceRoll() {
+//CREATES A RANDOM NUMBER METHOD Dice TO USE .roll
+//..ANYWHERE IN THE APPLICATION
+Dice.prototype.roll = function diceRoll() {
 	var randomNumber = Math.floor(Math.random() * this.sides + 1);
 	return randomNumber;
-}
+};
 
 //CONSTRUCTOR FUNCTION TO CALL DICE AND ROLL.
 function Dice(sides) {
 	this.sides = sides;
-	this.roll = diceRoll;
 }
 var diceOne = new Dice(6);
 var diceTwo = new Dice(6);
@@ -37,7 +36,6 @@ rollBothDiceBtn.onclick = function () {
 	printNumberTwo("<h1>" + rollTwo + "</h1>");
 };
 
-// JAVASCRIPT CLICK FUNCTION
 diceBtnOne.onclick = function() {
 	var rollOne = diceOne.roll();
 	printNumberOne("<h1>" + rollOne + "</h1>");
